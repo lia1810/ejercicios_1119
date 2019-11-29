53c4cf71042bb2779e1dae0484f81186d911f6cc
lia1810 <lbelliski@gmail.com>
2019-11-29 18:53:55 -0300

    primer commit

---
+++ ejercicio.js
@@ -0,0 +1,23 @@
+// let fibo = () => {
+//     let a = 0
+//     let b = 1
+//     let c
+//     let s = 1
+
+//     console.log(a);
+//     console.log(b);
+//     for (i = 3; i <= 10; i++) {
+//         c = a + b;
+//         console.log(c);
+//         s = s + c;
+//         a = b;
+//         b = c;
+//     }
+
+//     let fibo = () => {
+//         console.log(i)
+//     }
+// }
+// fibo();
+// fibo();
+// fibo();
